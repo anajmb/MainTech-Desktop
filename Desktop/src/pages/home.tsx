ea60c375225dc88ed0f95a41cb57cc4a88f4984a
Gabrielyfs <felixdesouzagabriely@gmail.com>
2025-10-04 14:30:49 -0300

    terminando página home

--- Desktop/src/pages/home.tsx
+++ Desktop/src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { Calendar, Grid2X2Plus, Plus, UserPlus } from "lucide-react";
+import { Calendar, CheckCircle, CheckCircle2Icon, CheckCircleIcon, ClockFading, FileText, Grid2X2Plus, ListCheck, Logs, Plus, PlusIcon, UserPlus } from "lucide-react";
 import Card from "../components/card";
 import Sidebar from "../components/sidebar";
 import "../styles/home.css"
@@ -9,9 +9,9 @@ export default function Home() {
         <div className="containerGeral">
             <Sidebar />
             <div className="containerPage">
-                <div>
+                {/* <div> */}
                     <Header />
-                </div>
+                {/* </div> */}
                 <h2 className="tituloBemVindo">Bem-Vindo, </h2>
 
                 <div className="containerCards">
@@ -26,30 +26,46 @@ export default function Home() {
                         <div className="acoesDashboards">
                             <div style={{ display: 'flex', flexDirection: 'row', gap: '2em', flexWrap: 'wrap', justifyContent: 'space-evenly', alignItems: 'center' }}>
                                 <Card>
-                                    <div className="acoesSubCard">
-                                        <Plus size={50} strokeWidth={1.5} color="#CE221E" />
-                                        <h3 className="acoesSubTitulo">Nova Tarefa</h3>
+                                    <div className="acoesDashboardsCard">
+                                        <h3 className="acoesDashboardsTitulo">Tarefas</h3>
+                                        <div className="acoesDashboardsCentro">
+                                            <CheckCircle size={20} strokeWidth={2} color="#36A23D" />
+                                            <h3 className="acoesDashboardsValue">85%</h3>
+                                        </div>
+                                        <h3 className="acoesDashboardsLabel">Concluídas</h3>
                                     </div>
                                 </Card>
 
                                 <Card >
-                                    <div className="acoesSubCard">
-                                        <Calendar size={38} strokeWidth={1.5} color="#438BE9" />
-                                        <h3 className="acoesSubTitulo">Agenda</h3>
+                                    <div className="acoesDashboardsCard">
+                                        <h3 className="acoesDashboardsTitulo">Total de Tarefas</h3>
+                                        <div className="acoesDashboardsCentro">
+                                            <ListCheck size={20} strokeWidth={1.5} color="#9500FF" />
+                                            <h3 className="acoesDashboardsValue">15</h3>
+                                        </div>
+                                        <h3 className="acoesDashboardsLabel">Neste mês</h3>
                                     </div>
                                 </Card>
 
                                 <Card >
-                                    <div className="acoesSubCard">
-                                        <UserPlus size={38} strokeWidth={1.5} color="#C300C7" />
-                                        <h3 className="acoesSubTitulo">Criar Usuário</h3>
+                                    <div className="acoesDashboardsCard">
+                                        <h3 className="acoesDashboardsTitulo">Tempo Médio</h3>
+                                        <div className="acoesDashboardsCentro">
+                                            <ClockFading size={20} strokeWidth={1.5} color="#4147D5" />
+                                            <h3 className="acoesDashboardsValue">5 min</h3>
+                                        </div>
+                                        <h3 className="acoesDashboardsLabel">Checklist</h3>
                                     </div>
                                 </Card>
 
                                 <Card >
-                                    <div className="acoesSubCard">
-                                        <Grid2X2Plus size={38} strokeWidth={1.5} color="#70CC88" />
-                                        <h3 className="acoesSubTitulo">Máquinas</h3>
+                                    <div className="acoesDashboardsCard">
+                                        <h3 className="acoesDashboardsTitulo">O.S.</h3>
+                                        <div className="acoesDashboardsCentro">
+                                            <FileText size={20} strokeWidth={1.5} color="#DD78BB" />
+                                            <h3 className="acoesDashboardsValue">8</h3>
+                                        </div>
+                                        <h3 className="acoesDashboardsLabel">Neste mês</h3>
                                     </div>
                                 </Card>
                             </div>
@@ -62,33 +78,42 @@ export default function Home() {
                             <h3 className="tituloCard">Ações Rápidas</h3>
 
                             <div style={{ display: 'flex', flexDirection: 'row', gap: '2em', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
-                                <Card>
-                                    <div className="acoesSubCard">
-                                        <Plus size={50} strokeWidth={1.5} color="#CE221E" />
-                                        <h3 className="acoesSubTitulo">Nova Tarefa</h3>
-                                    </div>
-                                </Card>
 
-                                <Card >
-                                    <div className="acoesSubCard">
-                                        <Calendar size={38} strokeWidth={1.5} color="#438BE9" />
-                                        <h3 className="acoesSubTitulo">Agenda</h3>
-                                    </div>
-                                </Card>
+                                <div style={{ cursor: 'pointer' }}>
+                                    <Card>
+                                        <div className="acoesSubCard" style={{gap: '0.9em'}}>
+                                            <PlusIcon size={45} strokeWidth={1.5} color="#CE221E" />
+                                            <h3 className="acoesSubTitulo">Nova Tarefa</h3>
+                                        </div>
+                                    </Card>
+                                </div>
 
-                                <Card >
-                                    <div className="acoesSubCard">
-                                        <UserPlus size={38} strokeWidth={1.5} color="#C300C7" />
-                                        <h3 className="acoesSubTitulo">Criar Usuário</h3>
-                                    </div>
-                                </Card>
+                                <div style={{ cursor: 'pointer' }}>
+                                    <Card >
+                                        <div className="acoesSubCard">
+                                            <Calendar size={32} strokeWidth={1.5} color="#438BE9" />
+                                            <h3 className="acoesSubTitulo">Agenda</h3>
+                                        </div>
+                                    </Card>
+                                </div>
 
-                                <Card >
-                                    <div className="acoesSubCard">
-                                        <Grid2X2Plus size={38} strokeWidth={1.5} color="#70CC88" />
-                                        <h3 className="acoesSubTitulo">Máquinas</h3>
-                                    </div>
-                                </Card>
+                                <div style={{ cursor: 'pointer'}}>
+                                    <Card >
+                                        <div className="acoesSubCard">
+                                            <UserPlus size={32} strokeWidth={1.5} color="#C300C7" />
+                                            <h3 className="acoesSubTitulo">Criar Usuário</h3>
+                                        </div>
+                                    </Card>
+                                </div>
+
+                                <div style={{ cursor: 'pointer' }}>
+                                    <Card >
+                                        <div className="acoesSubCard">
+                                            <Grid2X2Plus size={32} strokeWidth={1.5} color="#70CC88" />
+                                            <h3 className="acoesSubTitulo">Máquinas</h3>
+                                        </div>
+                                    </Card>
+                                </div>
                             </div>
                         </div>
 
