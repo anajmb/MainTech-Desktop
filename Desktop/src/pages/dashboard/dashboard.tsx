c888e0e9862bc68b65d951666df52602908296f4
Gabrielyfs <felixdesouzagabriely@gmail.com>
2025-10-04 17:56:58 -0300

    estilizando página dashboard

--- Desktop/src/pages/dashboard/dashboard.tsx
+++ Desktop/src/pages/dashboard/dashboard.tsx
@@ -1,3 +1,4 @@
+import CardBranco from "../../components/cardBranco";
 import Header from "../../components/header";
 import Sidebar from "../../components/sidebar";
 
@@ -7,9 +8,14 @@ export default function Dashboard() {
             <Sidebar />
             <div className="containerPage">
                 <Header />
+                <h2 className="tituloPage">Dashboard </h2>
 
                 <div className="containerCards">
-
+                    <CardBranco>
+                        <div className="topoDashboardCards">
+                
+                        </div>
+                    </CardBranco>
                 </div>
             </div>
         </div>
