5d7f995cf9d751e059e1bc0faae7ffab2857cb0e
Gabrielyfs <felixdesouzagabriely@gmail.com>
2025-10-05 00:07:28 -0300

    estilizando a página configurações

--- Desktop/src/pages/configuracoes/configuracoes.tsx
+++ Desktop/src/pages/configuracoes/configuracoes.tsx
@@ -1,5 +1,7 @@
+import CardBranco from "../../components/cardBranco";
 import Header from "../../components/header";
 import Sidebar from "../../components/sidebar";
+import '../../styles/configuracoes.css'
 
 export default function Configuracoes() {
 
@@ -11,7 +13,45 @@ export default function Configuracoes() {
                 <h2 className="tituloPage">Configurações </h2>
 
                 <div className="containerCards">
+                    <div className="contaCard">
+                        <CardBranco >
+                            <div>
+                                <h3 className="">Conta</h3>
+                                <div>
 
+                                </div>
+
+                                <h3>Preferência</h3>
+                                <div>
+
+                                </div>
+
+                                <h3>Suporte</h3>
+                                <div>
+
+                                </div>
+                            </div>
+                        </CardBranco>
+                    </div>
+
+                    <div className="perfilCard">
+                        <CardBranco>
+                            <h3 className="tituloCard">Minhas Informações</h3>
+
+                            <div className="inputsConfiguracoes">
+                                <input type="text" readOnly/>
+                                <input type="text" readOnly/>
+                                <input type="text" readOnly/>
+                                <input type="email" />
+                                <input type="text" />
+                            </div>
+
+                            <div>
+                                <button>Salvar</button>
+                                <button>Descartar Alterações</button>
+                            </div>
+                        </CardBranco>
+                    </div>
                 </div>
             </div>
         </div>
