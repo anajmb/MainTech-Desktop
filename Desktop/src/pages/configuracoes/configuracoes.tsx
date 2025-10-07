745924f6b061af6d503c27e1dfd9cf55b003fb44
beaPaludetto <beatrizpalubarreto@gmail.com>
2025-10-07 13:37:04 -0300

    add estrutura de configurações

--- Desktop/src/pages/configuracoes/configuracoes.tsx
+++ Desktop/src/pages/configuracoes/configuracoes.tsx
@@ -18,17 +18,27 @@ export default function Configuracoes() {
                             <div>
                                 <h3 className="">Conta</h3>
                                 <div>
-
+                                    {/* icon */}
+                                    <h3>Privacidade e Segurança</h3>
+                                    <h3>Gerenciar senha</h3>
                                 </div>
 
                                 <h3>Preferência</h3>
                                 <div>
+                                    <div>
+                                        <h3>Notificações</h3>
+                                        <h3>Controla alerta e avisos</h3>
+                                    </div>
 
+                                    <div>
+                                        <h3>Acessibilidade</h3>
+                                    </div>
                                 </div>
 
                                 <h3>Suporte</h3>
                                 <div>
-
+                                    <h3>Ajuda e Suporte</h3>
+                                    <h3>Central de ajuda e FAQ</h3>
                                 </div>
                             </div>
                         </CardBranco>
@@ -38,13 +48,30 @@ export default function Configuracoes() {
                         <CardBranco>
                             <h3 className="tituloCard">Minhas Informações</h3>
 
+                            <div>
+                                {/* foto */}
+                                <h3>João Silva de Souza Toledo</h3>
+
+                                <div>
+                                    <h3>Equipe Administrativa</h3>
+
+                                    <div>
+                                        <h3>Lider Técnico</h3>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <button>Editar foto</button>
+                                </div>
+                            </div>
+
                             <div className="inputsConfiguracoes">
-                                <input type="text" readOnly/>
-                                <input type="text" readOnly/>
-                                <input type="text" readOnly/>
+                                <input type="text" readOnly />
+                                <input type="text" readOnly />
+                                <input type="text" readOnly />
                                 <input type="email" />
                                 <input type="text" />
-                            </div>
+                            </div>  
 
                             <div>
                                 <button>Salvar</button>
