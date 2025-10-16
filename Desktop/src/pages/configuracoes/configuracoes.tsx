c053028b9ac1d31e0b4ab4d834e1d1fc8284eef7
Gabrielyfs <felixdesouzagabriely@gmail.com>
2025-10-16 16:18:30 -0300

    estilizando página Configurações

--- Desktop/src/pages/configuracoes/configuracoes.tsx
+++ Desktop/src/pages/configuracoes/configuracoes.tsx
@@ -1,7 +1,9 @@
+import { SquarePen } from "lucide-react";
 import CardBranco from "../../components/cardBranco";
 import Header from "../../components/header";
 import Sidebar from "../../components/sidebar";
 import '../../styles/configuracoes.css'
+import RandomColor from "../../hooks/randomColor";
 
 export default function Configuracoes() {
 
@@ -46,42 +48,59 @@ export default function Configuracoes() {
 
                     <div className="containerPerfilCard">
                         <CardBranco>
-                            <div className="perfilCard">
-                                <div>
+                            <div className="cardPage">
 
-                                    <h3 className="tituloCard">Minhas Informações</h3>
+                                <h3 className="tituloCard">Minha informações</h3>
+
+                                <div style={{ padding: '0px 40px', flex: 1 }}>
 
                                     <div className="headerPerfil">
-                                        {/* foto */}
-                                        <h3 className="nomePerfil">João Silva de Souza Toledo</h3>
+                                        <img src="" alt="" />
 
-                                        <div>
-                                            <h3>Equipe Administrativa</h3>
+                                        <div className="headerInfo">
+                                            <h3 className="nomeUser">João Silva de Souza Toledo</h3>
 
                                             <div>
-                                                <h3>Lider Técnico</h3>
+                                                <h5 className="equipe">Equipe Administrativa</h5>
+                                                <div style={{backgroundColor: RandomColor()}}>
+                                                    <h6 className="cargo">
+                                                    </h6>
+                                                </div>
                                             </div>
-                                        </div>
 
-                                        <div className="">
-                                            <button className="botaoEditar">Editar foto</button>
+                                            <button>Editar foto</button>
                                         </div>
                                     </div>
+                                    <div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
 
+                                        <div className="grupoInputIcon">
+                                            <input type="text" name="nome" id="nome" placeholder="Digite o Título da tarefa" readOnly value={"João de silva toledo"} className="inputIcon" />
+                                        </div>
+
+                                        <div className="grupoInputIcon">
+                                            <input type="text" name="cpf" id="cpf" readOnly value={'CPF'} className="inputIcon" />
+                                        </div>
+
+                                        <div className="grupoInputIcon">
+                                            <input type="data" name="nascimento" id="nascimento" readOnly value={"Data de Nascimento"} className="inputIcon" />
+                                        </div>
+
+                                        <div className="grupoInputIcon">
+                                            <input type="email" name="email" id="email" value={"email"} className="inputIcon" />
+                                            <SquarePen size={20} className="iconDoInput" />
+                                        </div>
+
+                                        <div className="grupoInputIcon">
+                                            <input type="tel" name="telefone" id="telefone" value={"Telefone"} className="inputIcon" />
+                                            <SquarePen size={20} className="iconDoInput" />
+                                        </div>
+                                    </div>
+                                    <div className="btnDiv" >
+                                        <button type="submit" className="btn">Salvar</button>
+                                        <button type="submit" className="btn" style={{ color: '#5c5c5c', backgroundColor: '#c5c5c5' }}>Descartar Alterações</button>
+                                    </div>
                                 </div>
 
-                                <div className="inputsConfiguracoes">
-                                    <input type="text" readOnly />
-                                    <input type="text" readOnly />
-                                    <input type="text" readOnly />
-                                    <input type="email" />
-                                    <input type="text" />
-                                </div>
-
-                                <div className="botoesPerfil">
-                                    <button className="botao salvar">Salvar</button>
-                                    <button className="botao descartar">Descartar Alterações</button>
-                                </div>
                             </div>
                         </CardBranco>
                     </div>
